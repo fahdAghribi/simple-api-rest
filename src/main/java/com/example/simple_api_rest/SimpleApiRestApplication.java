f8cddeeb94eddecfac693648b637d4cfdeed5d11
fahdAghribi <fahdseo@gmail.com>
2024-05-29 22:18:59 +0200

    Initial commit

---
+++ src/main/java/com/example/simple_api_rest/SimpleApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simple_api_rest/SimpleApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
